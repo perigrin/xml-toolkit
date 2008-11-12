9df9d08ff7b1a4cb45d9aed7899d3e41e792279b
Chris Prather <chris@prather.org>
2008-11-12 22:25:27 +0000

    add docs

--- lib/XML/Toolkit.pm
+++ lib/XML/Toolkit.pm
@@ -1,17 +1,13 @@
 package XML::Toolkit;
 use strict;
 our $VERSION = '0.0.1';
-use Moose;
 
-# Module implementation here
-
-no Moose; # unimport Moose's keywords so they won't accidentally become methods
 1;    # Magic true value required at end of module
 __END__
 
 =head1 NAME
 
-XML::Toolkit - [One line description of module's purpose here]
+XML::Toolkit - A set of tools for dealing with XML with the Way of the Moose.
 
 
 =head1 VERSION
@@ -21,98 +17,48 @@ This document describes XML::Toolkit version 0.0.1
 
 =head1 SYNOPSIS
 
-    use XML::Toolkit;
-
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
+  use XML::Toolkit::Loader;
+  my $loader = XML::Toolkit::Loader->new( namespace => $namespace );
+  $loader->parse_file( $$file );
+  print join '', @{ $loader->render };
   
   
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
+XML::Toolkit is less a specific module as it is a suite of related modules 
+that provide a way to interact with XML more like an ORM. 
 
+THIS RELEASE IS CONSIDERED ALPHA QUALITY. USE IT AT YOUR OWN RISK. 
 
-=head1 INTERFACE 
-
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
-
-
-=head1 DIAGNOSTICS
-
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
+There are no significant tests (beyond bare bones functional ones), and no
+significant documentation (beyond what you're reading now). 
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
 XML::Toolkit requires no configuration files or environment variables.
 
 
 =head1 DEPENDENCIES
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
+'Moose', 'Moose::Autobox','MooseX::AttributeHelpers', 'XML::SAX',
+'MooseX::Types::Path::Class', 'XML::SAX::Writer', 'Encode', 'Template'
 
 =head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
 
 None reported.
 
 
 =head1 BUGS AND LIMITATIONS
 
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
+THIS IS ALPHA QUALITY. USE AT YOUR OWN RISK. 
+
+While XML::Toolkit has been used in two projects in production, it is still
+considered alpha and may cause monkeys to fly out of delicate places. 
+
+Currently it only supports Auto-Discovery of class hierarchy from XML
+instances, and does not know anything about DTDs, RelaxNG, XML-Schema or
+anything else. Future work is being considered in this, and patches may be
+accepted if they come with documentation and tests.
 
 No bugs have been reported.
 
@@ -128,7 +74,7 @@ Chris Prather  C<< <perigrin@cpan.org> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright (c) 2008, Chris Prather C<< <perigrin@cpan.org> >>. All rights reserved.
+Copyright (c) 2008, Chris Prather C<< <perigrin@cpan.org> >>. Some rights reserved.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
