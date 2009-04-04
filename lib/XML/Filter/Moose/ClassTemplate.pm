6daf25223661eddccf681c15a435d68769af4f3c
Chris Prather <chris@prather.org>
2009-04-04 13:09:28 -0400

    update the default template

--- lib/XML/Filter/Moose/ClassTemplate.pm
+++ lib/XML/Filter/Moose/ClassTemplate.pm
@@ -9,39 +9,41 @@ has template => (
 
 sub _build_template {
     return <<'END_TEMPLATE'
-    # [% meta.name.replace('::', '/') %].pm
-    package [% meta.name %];
-    use Moose;
-    use MooseX::AttributeHelpers;
 
-    [% FOREACH attr_name IN meta.get_attribute_list.sort -%]
-    [%- attr = meta.get_attribute(attr_name) -%]
-    has '[% attr_name %]' => (
-            isa         => '[% attr.type_constraint.name %]',
-            is          => '[% IF attr.has_accessor %]rw[% ELSE %]ro[%END%]',
+package [% meta.name %];
+use Moose;
+use MooseX::AttributeHelpers;
 
-        [%- IF attr.type_constraint.is_subtype_of("ArrayRef") -%]
-            metaclass   => 'Collection::Array',
-            lazy        => 1,
-            auto_deref  => 1,
-            default     => sub { [] },
-            provides    => { push => '[% attr_name.remove("_collection") %]' },
-            description => {
-                sort_order => [% loop.index() %],
-            },
-    [% ELSE -%]
-        metaclass   => 'MooseX::MetaDescription',
-        description => {
-    [% FOREACH name IN attr.description.keys -%]
-            [% name %] => "[% attr.description.$name %]",
-    [% END -%]
-            sort_order => [% loop.index() %],
-        },
-    [% END -%]
-    );
-    [% END -%]
+[% FOREACH attr_name IN meta.get_attribute_list.sort -%]
+[% attr = meta.get_attribute(attr_name) -%]
+has '[% attr_name %]' => (
+     isa         => '[% attr.type_constraint.name %]',
+     is          => '[% IF attr.has_accessor %]rw[% ELSE %]ro[%END%]',
+     traits      => [ 'MooseX::MetaDescription::Meta::Trait' ],
+ [%- IF attr.type_constraint.is_subtype_of("ArrayRef") -%]
+     metaclass   => 'Collection::Array',
+     lazy        => 1,
+     auto_deref  => 1,
+     default     => sub { [] },
+     provides    => { push => '[% attr_name.remove("_collection") %]' },
+     description => {
+         sort_order => [% loop.index() %],
+     },
+[% ELSE -%]
+ description => {
+[% FOREACH name IN attr.description.keys -%]
+     [% name %] => "[% attr.description.$name %]",
+[% END -%]
+     sort_order => [% loop.index() %],
+ },
+[% END -%]
+);
+[% END -%]
 
-    no Moose;
+no Moose;
+1;
+__END__
+[% END %]
 
 END_TEMPLATE
 
