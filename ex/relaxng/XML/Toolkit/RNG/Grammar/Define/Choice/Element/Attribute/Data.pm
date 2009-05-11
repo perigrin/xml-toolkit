b4d9b7c77bef502a06cb3ec2bc42e8de09efd117
Chris Prather <chris@prather.org>
2009-05-10 23:57:56 -0400

    stub out documentation for RNG

--- ex/relaxng/XML/Toolkit/RNG/Grammar/Define/Choice/Element/Attribute/Data.pm
+++ ex/relaxng/XML/Toolkit/RNG/Grammar/Define/Choice/Element/Attribute/Data.pm
@@ -19,3 +19,51 @@ has 'type' => (
 no Moose;
 1;
 __END__
+
+__END__
+
+=head1 NAME
+
+XML::Toolkit::RNG::Grammar::Define::Choice::Element::Attribute::Data - A class to ...
+
+=head1 VERSION
+
+This documentation refers to version 0.01.
+
+=head1 SYNOPSIS
+
+use XML::Toolkit::RNG::Grammar::Define::Choice::Element::Attribute::Data;
+
+=head1 DESCRIPTION
+
+The XML::Toolkit::RNG::Grammar::Define::Choice::Element::Attribute::Data class implements ...
+
+=head1 SUBROUTINES / METHODS
+
+=head1 DEPENDENCIES
+
+Modules used, version dependencies, core yes/no
+
+Moose
+
+MooseX::AttributeHelpers
+
+=head1 NOTES
+
+...
+
+=head1 BUGS AND LIMITATIONS
+
+None known currently, please email the author if you find any.
+
+=head1 AUTHOR
+
+Chris Prather (perigrin@domain.tld)
+
+=head1 LICENCE
+
+Copyright 2009 by Chris Prather.
+
+This software is free.  It is licensed under the same terms as Perl itself.
+
+=cut
--- ex/relaxng/XML/Toolkit/RNG/Grammar/Define/Interleave/ZeroOrMore/Choice/Element/Attribute.pm
+++ ex/relaxng/XML/Toolkit/RNG/Grammar/Define/Interleave/ZeroOrMore/Choice/Element/Attribute.pm
@@ -31,3 +31,51 @@ has 'name' => (
 no Moose;
 1;
 __END__
+
+__END__
+
+=head1 NAME
+
+XML::Toolkit::RNG::Grammar::Define::Interleave::ZeroOrMore::Choice::Element::Attribute - A class to ...
+
+=head1 VERSION
+
+This documentation refers to version 0.01.
+
+=head1 SYNOPSIS
+
+use XML::Toolkit::RNG::Grammar::Define::Interleave::ZeroOrMore::Choice::Element::Attribute;
+
+=head1 DESCRIPTION
+
+The XML::Toolkit::RNG::Grammar::Define::Interleave::ZeroOrMore::Choice::Element::Attribute class implements ...
+
+=head1 SUBROUTINES / METHODS
+
+=head1 DEPENDENCIES
+
+Modules used, version dependencies, core yes/no
+
+Moose
+
+MooseX::AttributeHelpers
+
+=head1 NOTES
+
+...
+
+=head1 BUGS AND LIMITATIONS
+
+None known currently, please email the author if you find any.
+
+=head1 AUTHOR
+
+Chris Prather (perigrin@domain.tld)
+
+=head1 LICENCE
+
+Copyright 2009 by Chris Prather.
+
+This software is free.  It is licensed under the same terms as Perl itself.
+
+=cut
